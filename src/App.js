cbe9caf1e4fc5e7531fef7e83903fa1d3405f1ab
L1nkolnKr <mr.aleks111@gmail.com>
2021-09-13 23:24:10 +0300

    component drawer header card

--- src/App.js
+++ src/App.js
@@ -1,83 +1,25 @@
-import logo from './logo.svg';
+
+
+import Card from './components/Card'
 import './App.css';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
 function App() {
   return (
     <div className="wrapper clear">
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png"/>
-        <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кросовок</p>
-        </div>
-        </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-          <img width={18} height={17} src="/img/cart.svg"/>
-            <span>1205 grn.</span>
-            </li>
-          <li>
-          <img width={18} height={17} src="/img/user.svg"/>
-          </li>
-      
-        </ul>
-      </header>
+      <Drawer/>
+      <Header/>
       <div className="content p-40">
-        <h1 className= "mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+        <h1>Все кроссовки</h1>
+        <div className="search-block d-flex">
+          <img width={13} height={13} src="/img/search.svg" alt="Search"/>
+          <input placeholder="Поиск"/>
+        </div>
+        </div>
         <div className="d-flex">
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers"/>
-          <h5>Мужские кроссовки Nike</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>3199 grn.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers"/>
-          <h5>Мужские кроссовки Nike</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>3199 grn.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers"/>
-          <h5>Мужские кроссовки Nike</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>3199 grn.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/5.jpg" alt="Sneakers"/>
-          <h5>Мужские кроссовки Nike</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>3199 grn.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-            </button>
-          </div>
-        </div>
+          <Card/>
         </div>
       </div>
     </div>
